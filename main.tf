224b112d1e6dd01826dd3e7d1c06fbf65c252cb5
kiran7028 <kiran7028@gmail.com>
2025-12-03 15:14:32 +0530

    image added

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "tf-server" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-0d176f79571d18a8f"
   instance_type = "t3.micro"
   tags = {
     Name = "TerraformServerInstance"
   }
-  
+
 }
